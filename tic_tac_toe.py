a6e4a8aa417dd8426206cc5ee2740237dab00321
Noelle Chang <chang.noelle10@gmail.com>
2024-12-17 10:06:22 +0800

    print_board function takes 9 arguments, one for each space on the board

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,13 +1,16 @@
-def print_board():
+def print_board(a1 = '-', a2 = '-', a3= '-', b1= '-', b2= '-', b3= '-', c1= '-', c2= '-', c3= '-'):
     print("""   a     b     c
       |     |     
-1  -  |  -  |  -  
+1  {0}  |  {3}  |  {6}  
  _____|_____|_____
       |     |     
-2  -  |  -  |  -  
+2  {1}  |  {4}  |  {7}  
  _____|_____|_____
       |     |     
-3  -  |  -  |  -  
-      |     |     """)
+3  {2}  |  {5}  |  {8}  
+      |     |     """.format(a1, a2, a3, b1, b2, b3, c1, c2, c3))
 
 print_board()
+p1_name = input("Enter player 1 (O) name: ")
+p2_name = input("Enter player 2 (X) name: ")
+
